36a63b6b1ecc6d778634d204444a675e010099b7
release-agent <release-agent@stellar-mls.local>
2026-04-27 18:38:08 +0000

    release: v1.10.39 (PR #155)

--- clients/android/StellarChat/app/build.gradle.kts
+++ clients/android/StellarChat/app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "chat.onym.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 82
-        versionName = "1.10.38"
+        versionCode = 83
+        versionName = "1.10.39"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
 
